9d133f36c2a59404c0f7411871938bface9eb0d3
batustun <44682281+batustun@users.noreply.github.com>
2020-06-16 12:03:49 +0300

    Update slave-arduino.ino

--- slave-arduino.ino
+++ slave-arduino.ino
@@ -19,29 +19,29 @@ boolean fan_durumu = LOW;
 
 void setup()
 {
-  Wire.begin();
+  Wire.begin(1);
   /* I2C haberleşmesi master olarak başlatıldı */
   pinMode(fan_rolesi_pini, OUTPUT);
   Serial.begin(9600);
-  /* Bilgisayara veri yazdırabilmek için seri haberleşme başlatıldı */
+  Wire.onReceive(veriGeldiginde);
   servo_9.attach(9);
   servo_6.attach(6);
   cli();
 
-  // Timer2 kesmesi ayarlanıyor
-  TCNT2  = 1;
-  TCCR2A = 1;
-  TCCR2B = 1;
-  OCR2A = 15624;  // 1 saniye çalışması için gerekli zaman kesmesi frekansı
-  TCCR2B |= (1 << WGM22);
-  TCCR2B |= (1 << CS22) | (1 << CS20);
-  TIMSK2 |= (1 << OCIE2A);
-  sei();
+ // Timer2 kesmesi ayarlanıyor
+  TCNT0  = 0;
+  TCCR0A = 0;
+  TCCR0B = 0;
+  OCR0A = 15624;  // 1 saniye çalışması için gerekli zaman kesmesi frekansı
+  TCCR0B |= (0 << WGM02);
+  TCCR0B |= (0 << CS02) | (0 << CS20);
+  TIMSK0 |= (0 << OCIE0A);
+  sei();  
   
  
 }
 
-ISR(TIMER2_COMPA_vect){     //Her kesmeye girildiğinde otomatik çalıştırılacak fonksiyon.
+ISR(TIMER0_COMPA_vect){     //Her kesmeye girildiğinde otomatik çalıştırılacak fonksiyon.
  
  fan_durumu = !fan_durumu;
 
@@ -53,57 +53,54 @@ ISR(TIMER2_COMPA_vect){     //Her kesmeye girildiğinde otomatik çalıştırıl
 
 void loop()
 {
-  Wire.beginTransmission(1);
-  /* 1 adresine sahip Slave (köle) cihazına veri yollanacağı bildiriliyor */
-  
-  Wire.endTransmission();
-  /* Yollanacak verilerin bittiği bildiriliyor */
+ 
  
  
   
   //yumurta haznesinin düzenli bir şekilde dönmesini sağlar
-   for (pos = 0; pos <= 180; pos += 1) {
+  for (pos = 0; pos <= 180; pos += 1) {
    
-    servo_9.write(pos);
-    
+    servo_6.write(pos);   
+   
   }
-   delay(1000);
+ 
+    delay(1000);
+  
  for (pos = 180; pos >= 0; pos -= 1) {
    
-   servo_9.write(pos);
-     
-  }
-  delay(1000);
-  Wire.requestFrom(1,0x001);
-  //Wire.requestFrom(1,0x001);
- 
-  /* I2C hattından gelen veriler gelenKarakter değişkenine yazdırılacak */
+   servo_6.write(pos);
   
-  while(Wire.available()){
-    /* I2C hattında yeni veri olduğu sürece döngü devam edecek */
-   int sensoryuzdesi = Wire.read();
-    //sicaklik_degeri = Wire.read();
-    //voltaj_degeri = Wire.read();
- 
-    /* I2C hattından gelen veriler okunuyor */
-    Serial.print(sensoryuzdesi);
-    //Serial.print(sicaklik_degeri);
-    //Serial.print(voltaj_degeri);
-   if(sensoryuzdesi == 0){
+  }
+  delay(100);
+  
+  
+  
+  
+    /* Gelen veriler ekrana yazdırılıyor */
+  }
+
+
+   void veriGeldiginde(int veri)
+   {
+  
+  int sensoryuzdesi = Wire.read();
+  Serial.println(sensoryuzdesi);
+  int sicaklik_degeri = Wire.read();
+  Serial.println(sicaklik_degeri);
+  int voltaj_degeri = Wire.read();
+  Serial.println(voltaj_degeri);
+     
+if(sensoryuzdesi == 0 && sicaklik_degeri== 0 && voltaj_degeri== 0 ){
     
     servo_9.write(180);
     
-    delay(100);
+  
   }
     else
   {
    servo_9.write(0);
   }
-   
-    /* Gelen veriler ekrana yazdırılıyor */
-  }
-  Serial.println();
  
- 
-  
+     
 }
+  
